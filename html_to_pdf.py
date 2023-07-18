c633511d5ede29b8ea00d9708750a036cecf9ecd
Tim Zubov <timofey.zubov.05@gmail.com>
2023-07-18 21:40:03 +0300

    Make correct render of html to pdf

--- html_to_pdf.py
+++ html_to_pdf.py
@@ -1,9 +1,18 @@
 import pdfkit
 
 # with open('index.html', 'rb') as f:
+html = './docs/index.html'
+base_css = './docs/css/'
+css = [base_css + 'style.css', base_css + 'dark.css']
 pdfkit.from_file(
-    'index.html',
-    'test.pdf',
-    options={"enable-local-file-access": ""},
-    css=['style.css']
+    html,
+    'test-dark.pdf',
+    options={"enable-local-file-access": "",
+             "page-width": "0",
+             "margin-left": '0',
+             "margin-right": '0',
+             "margin-top": '0',
+             "margin-bottom": '0',
+             "disable-smart-shrinking": ""},
+    css=css
 )
